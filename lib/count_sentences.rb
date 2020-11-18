d7d346eb4a9d14c5b53b6da5fd7f8f332baf1421
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-18 15:18:14 -0800

    Monkey patches class=string with multiple instances

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true 
+      true
+    else 
+      false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?") == true 
+      true
+    else 
+      false 
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true 
+      true
+    else 
+      false 
+    end 
   end
 
   def count_sentences
-
+     array = self.split(/[.!?] /)
+     count = array.count
+     count
   end
 end
+
+
